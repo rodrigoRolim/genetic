2074f7f589cdc6b1bac91d12a7d3d5b4a241cb0e
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-01 18:35:23 -0300

    created fitness

--- benchmark_2/fitness.py
+++ benchmark_2/fitness.py
@@ -1,3 +1,16 @@
 class Fitness:
-  def __init__(self):
-    
+  fitness = 0
+  list_fitness = []
+  def setFitness(self, population):
+    sm = 0
+    pd = 1
+    for cromossoma in population:
+      for gene in cromossoma:
+        sm = gene + sm
+        pd = gene*pd
+      self.fitness = (3000 + 10**30) - (sm + pd)
+      self.list_fitness.append(self.fitness)
+  
+  def getFitness(self):
+    return self.list_fitness
+  
