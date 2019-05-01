00b2d2f830b91146cd0a7bc2ab5104b55cb7ba3e
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-01 19:36:56 -0300

    implemented roulette of selection class

--- benchmark_2/selection.py
+++ benchmark_2/selection.py
@@ -1,8 +1,12 @@
+import random
+
 class Selection:
+
   probabilities = []
   probability = 0
   acumulateds = [] #lista de probalidades acumuladas
- 
+  randomNumbers = []
+
   def calcule_probabilities(self, fitnessList):
     
     sumFitness = 0
@@ -19,3 +23,25 @@ class Selection:
       for j in range(i, -1, -1):
         sumProbs = self.probabilities[j] + sumProbs
         acumulateds.append(sumProbs)
+  
+  def generate_random_numbers(self, n):
+    # n é o tamanho da população
+    # gerar número aleatórios de 0 a 1
+    for i in range(n):
+      randomNumbers.append(round(random.uniform(-1, 1), 3))
+
+  def run_roulette(self, fitnessList, n):
+    indexs = []
+    
+    self.calcule_probabilities(fitnessList)
+    self.acumulated_probabilities()
+    self.generate_random_numbers(n)
+
+    for randomNumber in self.randomNumbers:
+      for acumulated in self.acumulateds:
+        if(acumulated >= randomNumber):
+          indexs.append(self.acumulateds.index(acumulated))
+          break
+
+    return indexs
+
