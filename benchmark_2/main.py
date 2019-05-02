92be196ee1721ba691f3df74bb468ab2b57682bf
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-01 22:01:25 -0300

    implemented prototype sample in main

--- benchmark_2/main.py
+++ benchmark_2/main.py
@@ -1,4 +1,18 @@
-from cromossoma import Cromossoma
+import random
+import numpy
+sm = 0
+pd = 1
+cromossoma = []
+for i in range(30):
+  gene = round(random.uniform(-11, 10), 3)
+  cromossoma.append(gene)
 
-cromossoma = Cromossoma()
-cromossoma.show()
+
+
+for gene in cromossoma:
+  sm = numpy.abs(gene) + sm
+  pd = numpy.abs(gene)*pd
+
+fitness = (3000 + 10**30) - (sm + pd)
+print(round(fitness, 3))
+print(cromossoma)
