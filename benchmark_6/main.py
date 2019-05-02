76e5d0a0da9fb8c7fa3e5dcd3d32a17eb8ecf243
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-01 22:00:31 -0300

    init benchmark_6

---
+++ benchmark_6/main.py
@@ -0,0 +1,18 @@
+import random
+import numpy
+sm = 0
+pd = 1
+cromossoma = []
+for i in range(30):
+  gene = round(random.uniform(-101, 100), 3)
+  cromossoma.append(gene)
+
+
+
+for gene in cromossoma:
+  sm = (gene + 0.5) + sm
+  
+
+fitness = 100*0.5 - sm 
+print(round(fitness, 3))
+print(cromossoma)
