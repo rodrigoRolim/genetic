2f8fa65ae07b7fe2327f908e039f26d6394a7b74
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-08 17:14:08 -0300

    refactory functions

--- benchmark_1/fitness.py
+++ benchmark_1/fitness.py
@@ -1,14 +1,17 @@
 class Fitness:
   fitness = 0
   list_fitness = []
-  def setFitness(self, population):
+  def set_fitness(self, population):
     sm = 0
     for cromossoma in population:
+     #print(cromossoma)
       for gene in cromossoma:
-        sm = gene**2 + sm
+        sm = (gene**2) + sm
       self.fitness = round(sm, 3)
       self.list_fitness.append(self.fitness)
-  
-  def getFitness(self):
+      self.fitness = 0
+  def get_fitness(self):
     return self.list_fitness
+  def empty_list_fitness(self):
+    self.list_fitness.clear()
   
