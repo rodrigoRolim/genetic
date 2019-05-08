67875c03b970829086d67d7c423a71a2a617c115
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-08 17:14:32 -0300

    created main

--- benchmark_1/main.py
+++ benchmark_1/main.py
@@ -3,32 +3,35 @@ from population import Population
 from fitness import Fitness
 from selection import Selection
 from reproduction import Reproduction
+import numpy as np
 
-p = Population()
-l = 0
+pop = []
+population = Population() 
+population.init_population()
+fit = Fitness()
+reproduction = Reproduction()
+selection = Selection()
+pop = population.get_population()
+i = 1
+while (True):
 
-while (l < 10):
-  c = Cromossoma()
-  p.setPopulation(c.show())
-  l = 1 + l
+  fit.set_fitness(pop.copy())
+  
+  if 300000 in fit.get_fitness():
+    print(fit.get_fitness())
+    print("achei")
+    break
+  selected_population = selection.get_new_population(fit.get_fitness().copy(), pop.copy())
+  new_pop = reproduction.crossover(selected_population.copy())
+  mutade_pop = reproduction.mutation(new_pop.copy())
+  pop.clear()
+  selected_population.clear()
+  new_pop.clear()
+  pop = mutade_pop.copy()
+  mutade_pop.clear()
+  #print(pop)
+  fit.empty_list_fitness()
+  i = i + 1
 
-# print(p.getPopulation())
-f = Fitness()
-f.setFitness(p.getPopulation())
 
-print(f.getFitness())
-
-x = f.getFitness()
-print(x[1])
-s = Selection()
-
-y = s.get_new_population(x, p.getPopulation())
-print(y)
-
-r = Reproduction()
-np = r.crossover(y)
-pm = r.mutation(np)
-print("pop cruzada")
-print(np)
-print("pop mutada")
-print(pm)
+  
