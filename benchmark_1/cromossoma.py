f8bfb6475f6404bcaab2ce818f00e01cf29063ae
rodrigoRolim <rodrigorolimveras92@gmail.com>
2019-05-22 21:54:43 -0300

    finish

--- benchmark_1/cromossoma.py
+++ benchmark_1/cromossoma.py
@@ -5,12 +5,9 @@ class Cromossoma:
   def make_cromossoma(self):
     for i in range(30):
       gene = round(random.uniform(-100, 100), 3)
-      #print(gene)
       self.cromossoma.append(gene)
     return self.cromossoma
   
   def empty(self):
     self.cromossoma.clear()
 
-#c = Cromossoma()
-#print(c.make_cromossoma())
